ea9981b99e72adf388cb475047ab80977b4ec601
Ta Duc Dung <taducdung1995@gmail.com>
2017-10-10 13:17:06 +0700

    add-my-review

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :reviews
+  resources :reviews do
+    get 'my_review', on: :collection
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
