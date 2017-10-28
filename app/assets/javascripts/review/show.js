49123dce43bf39ceaa874d0ebd0c52cd991716a6
Long Nguyen <longnp95@gmail.com>
2017-10-28 20:51:36 +0700

    comment & reply

--- app/assets/javascripts/review/show.js
+++ app/assets/javascripts/review/show.js
@@ -12,23 +12,75 @@ function showReview() {
         },
         mounted: function () {
             this.comment.user_id = $('textarea[name="comment"]').attr('data-current-user-id');
+            this.reply.user_id = $('textarea[name="comment"]').attr('data-current-user-id');
+            this.comment.review_id = $('textarea[name="comment"]').attr('data-review-id');
             this.fetchData();
         },
         methods: {
+            userLink: function (user_id) {
+                return '/user/' + user_id;
+            },
+            formatTime: function (time) {
+                let date = new Date(time);
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                let year = date.getFullYear();
+                return `${year}年${month}月${day}日`;
+            },
             fetchData: function () {
-                let url = location.href;
-                axios.get(url + '.json')
+                let url = location.href + '.json';
+                axios.get(url)
                     .then(res => {
-                        console.log(res.data);
+                        this.comments = res.data;
                     })
                     .catch(err => {
 
                     });
             },
+            addReply: function (reply) {
+                let idx = this.comments.findIndex(item => {
+                    return reply.comment_id === item.id;
+                });
+
+                if (idx !== -1) {
+                    this.comments[idx].replies.push(reply);
+                    this.comments.splice();
+                }
+            },
+            removeReply: function (comment_id, reply_id) {
+                let idx = this.comments.findIndex(item => {
+                    return comment_id === item.id;
+                });
+
+                if (idx !== -1) {
+                    let replies = this.comments[idx].replies;
+                    idx = replies.findIndex(item => {
+                        return item.id === reply_id;
+                    });
+
+                    if (idx !== -1) {
+                        replies.splice(idx, 1);
+                        this.comments.splice();
+                    }
+                }
+            },
+            removeComment: function (comment_id) {
+                let idx = this.comments.findIndex(item => {
+                    return comment_id === item.id;
+                });
+
+                if (idx !== -1) {
+                    this.comments.splice(idx, 1);
+                }
+            },
             postComment: function () {
-                let url = location.href + '.json';
-                axios.post('url', this.comment)
+                let url = '/comments.json';
+                axios.post(url, {
+                    comment: this.comment,
+                })
                     .then(res => {
+                        console.log(res.data);
+                        this.comments.push(res.data);
                         this.comment.content = '';
                         this.closeWriteCmtMode();
                     })
@@ -37,19 +89,58 @@ function showReview() {
             },
             postReply: function (comment_id) {
                 this.reply.comment_id = comment_id;
-                console.log(this.reply);
+                let url = '/replies.json';
+                axios.post(url, {
+                    reply: this.reply,
+                })
+                    .then(res => {
+                        this.addReply(res.data);
+                        this.reply.content = '';
+                    })
+                    .catch(err => {
+                    });
             },
-            deleteComment: function () {
+            deleteComment: function (comment_id) {
+                let url = '/comments/' + comment_id;
+                axios.delete(url)
+                    .then(res => {
+                        if (!res.data.error) {
+                            this.removeComment(comment_id);
+                        }
+                    })
+                    .catch(err => {
 
+                    });
             },
-            deleteReply: function () {
+            deleteReply: function (reply_id) {
+                let url = '/replies/' + reply_id;
+                axios.delete(url)
+                    .then(res => {
+                        if (!res.data.error) {
+                            comment_id = parseInt(res.data.comment_id);
+                            this.removeReply(comment_id, reply_id);
+                        }
+                    })
+                    .catch(err => {
 
+                    });
             },
             openWriteCmtMode: function () {
                 this.is_write_cmt = true;
             },
             closeWriteCmtMode: function () {
                 this.is_write_cmt = false;
+            },
+            openReply: function (comment_id) {
+                this.closeReply(this.reply.comment_id);
+                this.reply.comment_id = comment_id;
+                $('#r' + comment_id).removeClass('u-hide');
+            },
+            closeReply: function (comment_id) {
+                this.reply.content = '';
+                if (comment_id) {
+                    $('#r' + comment_id).addClass('u-hide');
+                }
             },
         },
     });
